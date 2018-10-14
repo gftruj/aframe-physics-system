495f1bb822826b18b7bbc421c4edfb9af19dcf7f
Piotr Milewski <gftruj@gmail.com>
2018-10-15 00:49:59 +0200

    added dist

--- dist/aframe-physics-system.js
+++ dist/aframe-physics-system.js
@@ -7,6 +7,7 @@ require('./src/components/body/dynamic-body');
 require('./src/components/body/static-body');
 require('./src/components/shape/shape')
 require('./src/components/constraint');
+require('./src/components/spring');
 require('./src/system');
 
 module.exports = {
@@ -18,7 +19,7 @@ module.exports = {
 // Export CANNON.js.
 window.CANNON = window.CANNON || CANNON;
 
-},{"./src/components/body/body":63,"./src/components/body/dynamic-body":64,"./src/components/body/static-body":65,"./src/components/constraint":66,"./src/components/math":67,"./src/components/shape/shape":69,"./src/system":79,"cannon":4}],2:[function(require,module,exports){
+},{"./src/components/body/body":63,"./src/components/body/dynamic-body":64,"./src/components/body/static-body":65,"./src/components/constraint":66,"./src/components/math":67,"./src/components/shape/shape":69,"./src/components/spring":70,"./src/system":80,"cannon":4}],2:[function(require,module,exports){
 /**
  * CANNON.shape2mesh
  *
@@ -16285,6 +16286,110 @@ module.exports.definition = Shape;
 module.exports.Component = AFRAME.registerComponent('shape', Shape);
 
 },{"cannon":4}],70:[function(require,module,exports){
+var CANNON = require('cannon');
+
+module.exports = AFRAME.registerComponent("spring", {
+
+  multiple: true,
+
+  schema: {
+    // Target (other) body for the constraint.
+    target: {type: 'selector'},
+
+    // Lenght of the spring, when no force acts upon it
+    restLength: {default: 1, min: 0},
+
+    // how much will the spring suppress the force
+    stiffness: {default: 100, min: 0, max: 0},
+
+    // the stretch factor of the spring
+    damping: {default: 1, min: 0, max: 1},
+
+    // offsets
+    localAnchorA: {type: 'vec3', default: {x: 0, y: 0, z: 0}},
+    localAnchorB: {type: 'vec3', default: {x: 0, y: 0, z: 0}},
+  },
+
+  init: function() {
+    this.system = this.el.sceneEl.systems.physics
+    this.world = this.system.driver.world
+    this.spring = /* {CANNON.Spring} */ null
+  },
+
+  update: function(oldData, newData) {
+    var el = this.el,
+    data = this.data;
+
+    if (!data.target) {
+      console.warn("Spring: invalid target specified.");
+      return; 
+    }
+    
+    // wait until the CANNON bodies is created and attached
+    if (!el.body || !data.target.body) {
+      (el.body ? data.target : el).addEventListener('body-loaded', this.update.bind(this, {}));
+      return;
+    }
+
+    // create the spring if necessary
+    this.createSpring()
+    // apply new data to the spring
+    this.updateSpring(oldData)
+  },
+
+  updateSpring: function(oldData) {
+    if (!this.spring) {
+      console.warn("Spring: Component attempted to change spring before its created. No changes made.");
+      return;
+    } 
+    var data = this.data,
+    spring = this.spring
+
+    // Cycle through the schema and check if an attribute has changed.
+    // if so, apply it to the spring
+    Object.keys(data).forEach(function(attr) {
+      if (data[attr] !== oldData[attr]) {
+        if (attr === "target") {
+          // special case for the target selector
+          spring.bodyB = data.target.body
+          return
+        }
+        spring[attr] = data[attr]
+      }
+    })
+  },
+
+  createSpring: function() {
+    if (this.spring) return // no need to create a new spring
+    this.spring = new CANNON.Spring(this.el.body);
+    // Compute the force after each step
+    this.world.addEventListener("postStep", this.updateSpringForce.bind(this, {}));
+  },
+
+  // If the spring is valid, update the force each tick the physics are calculated
+  updateSpringForce: function() {
+    return this.spring ? this.spring.applyForce() : void 0
+  },
+
+  // resume updating the force when component upon calling play()
+  play: function() {
+    this.world.addEventListener("postStep", this.updateSpringForce.bind(this, {}));
+  },
+
+  // stop updating the force when component upon calling stop()
+  pause: function() {
+    this.world.removeEventListener("postStep", this.updateSpringForce.bind(this, {}));
+  },
+
+  //remove the event listener + delete the spring
+  remove: function() {
+    this.world.removeEventListener("postStep", this.updateSpringForce.bind(this, {}));
+    if (this.spring)
+      delete this.spring
+      this.spring = null
+  }
+})
+},{"cannon":4}],71:[function(require,module,exports){
 module.exports = {
   GRAVITY: -9.8,
   MAX_INTERVAL: 4 / 60,
@@ -16299,7 +16404,7 @@ module.exports = {
   }
 };
 
-},{}],71:[function(require,module,exports){
+},{}],72:[function(require,module,exports){
 var Driver = require('./driver');
 
 function AmmoDriver () {
@@ -16311,7 +16416,7 @@ AmmoDriver.prototype.constructor = AmmoDriver;
 
 module.exports = AmmoDriver;
 
-},{"./driver":72}],72:[function(require,module,exports){
+},{"./driver":73}],73:[function(require,module,exports){
 /**
  * Driver - defines limited API to local and remote physics controllers.
  */
@@ -16389,7 +16494,7 @@ function abstractMethod () {
   throw new Error('Method not implemented.');
 }
 
-},{}],73:[function(require,module,exports){
+},{}],74:[function(require,module,exports){
 module.exports = {
   INIT: 'init',
   STEP: 'step',
@@ -16409,7 +16514,7 @@ module.exports = {
   REMOVE_CONSTRAINT: 'remove-constraint'
 };
 
-},{}],74:[function(require,module,exports){
+},{}],75:[function(require,module,exports){
 var CANNON = require('cannon'),
     Driver = require('./driver');
 
@@ -16530,7 +16635,7 @@ LocalDriver.prototype.getContacts = function () {
   return this.world.contacts;
 };
 
-},{"./driver":72,"cannon":4}],75:[function(require,module,exports){
+},{"./driver":73,"cannon":4}],76:[function(require,module,exports){
 var Driver = require('./driver');
 
 function NetworkDriver () {
@@ -16542,7 +16647,7 @@ NetworkDriver.prototype.constructor = NetworkDriver;
 
 module.exports = NetworkDriver;
 
-},{"./driver":72}],76:[function(require,module,exports){
+},{"./driver":73}],77:[function(require,module,exports){
 /**
  * Stub version of webworkify, for debugging code outside of a webworker.
  */
@@ -16585,7 +16690,7 @@ EventTarget.prototype.postMessage = function (msg) {
   this.target.dispatchEvent('message', {data: msg});
 };
 
-},{}],77:[function(require,module,exports){
+},{}],78:[function(require,module,exports){
 /* global performance */
 
 var webworkify = require('webworkify'),
@@ -16821,7 +16926,7 @@ WorkerDriver.prototype.getContacts = function () {
   });
 };
 
-},{"../utils/protocol":81,"./driver":72,"./event":73,"./webworkify-debug":76,"./worker":78,"webworkify":62}],78:[function(require,module,exports){
+},{"../utils/protocol":82,"./driver":73,"./event":74,"./webworkify-debug":77,"./worker":79,"webworkify":62}],79:[function(require,module,exports){
 var Event = require('./event'),
     LocalDriver = require('./local-driver'),
     AmmoDriver = require('./ammo-driver'),
@@ -16915,7 +17020,7 @@ module.exports = function (self) {
   }
 };
 
-},{"../utils/protocol":81,"./ammo-driver":71,"./event":73,"./local-driver":74}],79:[function(require,module,exports){
+},{"../utils/protocol":82,"./ammo-driver":72,"./event":74,"./local-driver":75}],80:[function(require,module,exports){
 var CANNON = require('cannon'),
     CONSTANTS = require('./constants'),
     C_GRAV = CONSTANTS.GRAVITY,
@@ -17160,7 +17265,7 @@ module.exports = AFRAME.registerSystem('physics', {
   }
 });
 
-},{"./constants":70,"./drivers/ammo-driver":71,"./drivers/local-driver":74,"./drivers/network-driver":75,"./drivers/worker-driver":77,"cannon":4}],80:[function(require,module,exports){
+},{"./constants":71,"./drivers/ammo-driver":72,"./drivers/local-driver":75,"./drivers/network-driver":76,"./drivers/worker-driver":78,"cannon":4}],81:[function(require,module,exports){
 module.exports.slerp = function ( a, b, t ) {
   if ( t <= 0 ) return a;
   if ( t >= 1 ) return b;
@@ -17225,7 +17330,7 @@ module.exports.slerp = function ( a, b, t ) {
 
 };
 
-},{}],81:[function(require,module,exports){
+},{}],82:[function(require,module,exports){
 var CANNON = require('cannon');
 var mathUtils = require('./math');
 
@@ -17552,4 +17657,4 @@ function deserializeQuaternion (message) {
   return new CANNON.Quaternion(message[0], message[1], message[2], message[3]);
 }
 
-},{"./math":80,"cannon":4}]},{},[1]);
+},{"./math":81,"cannon":4}]},{},[1]);
